406c25985e843b62a69ebc0b39da4484dd19bac5
Matt Ditmars <ditmarsmatt@gmail.com>
2020-11-08 18:31:35 -0500

    finished app implementation and styled it

--- src/App.js
+++ src/App.js
@@ -4,18 +4,20 @@ import 'firebase/auth';
 
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 
-firebase.initializeApp({
-  apiKey: "AIzaSyCwQ3oYRQwm_MxCoeImiWd4OHdO3H3G8mo",
-    authDomain: "chat-app-8a843.firebaseapp.com",
-    databaseURL: "https://chat-app-8a843.firebaseio.com",
-    projectId: "chat-app-8a843",
-    storageBucket: "chat-app-8a843.appspot.com",
-    messagingSenderId: "338751244028",
-    appId: "1:338751244028:web:6c28bfde4217e45bfdb979",
-    measurementId: "G-NM4TVHQK8X"
-})
+if(!firebase.apps.length) {
+  firebase.initializeApp({
+    apiKey: "AIzaSyCwQ3oYRQwm_MxCoeImiWd4OHdO3H3G8mo",
+      authDomain: "chat-app-8a843.firebaseapp.com",
+      databaseURL: "https://chat-app-8a843.firebaseio.com",
+      projectId: "chat-app-8a843",
+      storageBucket: "chat-app-8a843.appspot.com",
+      messagingSenderId: "338751244028",
+      appId: "1:338751244028:web:6c28bfde4217e45bfdb979",
+      measurementId: "G-NM4TVHQK8X"
+  });
+}
 
 const auth = firebase.auth();
 const firestore = firebase.firestore();
@@ -27,10 +29,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <h1>MattChat</h1>
+        { user ? <SignOut /> : <SignIn /> }
       </header>
       <section>
-        { user ? <ChatRoom /> : <SignIn /> }
+        { user ? <ChatRoom /> : <LoggedOutHome /> }
       </section>
     </div>
   );
@@ -43,17 +46,24 @@ function SignIn() {
   }
 
   return (
-    <button onClick={ signInWithGoogle}>Sign in with google</button>
+    <button className="sign-in" onClick={ signInWithGoogle }>Sign In</button>
   )
 }
 
 function SignOut() {
   return auth.currentUser && (
     <button onClick={() => auth.signOut()}>Sign Out</button>
+    )
+  }
+
+function LoggedOutHome() {
+  return (
+    <div className="logged-out-home">Sign in with your google account to access the chat room!</div>
   )
 }
-
-function ChatRoom() {
+  
+  function ChatRoom() {
+  const dummy = useRef();
   const messagesRef = firestore.collection('/messages');
   const query = messagesRef.orderBy('createdAt').limit(25);
 
@@ -73,17 +83,18 @@ function ChatRoom() {
         userId: uid,
       }
     );
+    dummy.current.scrollIntoView({ behavior: 'smooth' });
   };
 
   return (
     <div>
-      <SignOut />
-      <div>
+      <main>
         {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />) }
-      </div>
+        <span ref={dummy}></span>
+      </main>
       <form onSubmit={ sendMessage }>
-        <input value={formInput} onChange={ (e) => {setFormInput(e.target.value)}} />
-        <button type="submit">Send</button>
+        <input type="text" placeholder="Chat here..." value={formInput} onChange={ (e) => {setFormInput(e.target.value)}} />
+        <button type="submit" disabled={!formInput}>Send</button>
       </form>
     </div>
   )
@@ -91,10 +102,10 @@ function ChatRoom() {
 
 function ChatMessage(props) {
   const { text, userId, photoUrl } = props.message;
+  const messageClass = userId === auth.currentUser.uid ? 'sent' : 'received';
 
   return (
-    <div>
-
+    <div className={`message ${messageClass}`}>
       <img src={photoUrl} />
       <p>{ text }</p>
     </div>
